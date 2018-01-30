801a3eb17e2e7d724b38ab3602be3ad72b06c2b3
Clément Raussin <clement@raussin.com>
2018-01-30 20:15:58 +0100

    Maybe last route update fix...

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('update', 'UpdateController@index');
-Route::get('post', 'UpdateController@index');
+Route::post('update', 'UpdateController@index');
 
 //Routes when logged in
 Route::group(['middleware' => 'auth'], function()
